b730f919ba9c5bd553172ef38bc7d1aa166d34e4
mohilkhare1708 <mohilkhare17@gmail.com>
2021-07-02 23:45:04 +0530

    complete tictactoe

--- tictactoe/src/index.js
+++ tictactoe/src/index.js
@@ -1,24 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
 
-  render() {
-    return (
-      <button 
-        className="square" 
-        onClick = {() => this.setState({value: 'X'})}
-      >
-        {this.state.value}
-      </button>
-    );
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  )
+}
+
+function findWinner(squares) {
+  const wins = [
+    [0, 1, 2],
+    [0, 4, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8],
+  ];
+  for(let i = 0; i < wins.length; i++) {
+    const [a, b, c] = wins[i];
+    if(squares[a] && squares[b] === squares[b] && squares[c])
+      return squares[a];
   }
+  return null;
 }
 
 class Board extends React.Component {
@@ -26,16 +34,37 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
 
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    if(findWinner(squares) || squares[i])
+      return;
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares : squares,
+      xIsNext : !this.state.xIsNext,
+    });
+  }
+
   renderSquare(i) {
-    return <Square value = {i}/>;
+    return (
+      <Square 
+        value = {this.state.squares[i]}
+        onClick = {() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
-    const status = 'Next player: X';
-
+    const winner = findWinner(this.state.squares);
+    let status;
+    if(winner)
+      status = 'Winner: ' + winner;
+    else
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     return (
       <div>
         <div className="status">{status}</div>
